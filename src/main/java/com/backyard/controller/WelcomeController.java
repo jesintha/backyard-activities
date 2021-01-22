86cd23cb7464c31c4720a34fdb7eaaab15141d28
jesintha <jesintha.louis@gmail.com>
2021-01-22 15:49:04 -0600

    Adding configuration to load properties from spring cloud

--- src/main/java/com/backyard/controller/WelcomeController.java
+++ src/main/java/com/backyard/controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.backyard.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,12 @@ import com.backyard.beans.WelcomeBean;
 @RestController
 public class WelcomeController {
 	
+	@Value("${backyard.city}")
+	private String city;
+	
 	@GetMapping ("/welcome")
 	public String welcome() {
-		return "Welcome to Backyard Rulz";
+		return "Welcome to Backyard Rulz in "+ city;
 	}
 	
 	@GetMapping ("/welcome/{username}")
